00f60abe815041f7173c0cc49b94f88e23b2e758
Davit Gyulinyan <93977518+DavidGyulinyan@users.noreply.github.com>
2022-05-27 02:33:59 +0400

    Add files via upload

---
+++ SpaceX/js/script.js
@@ -0,0 +1,11 @@
+/* let title = document.querySelector('.header-main');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 70) {
+        title.style.cssText = 'position:fixed; top:10px';
+    }else {
+        // title.style.cssText = 'visibility:hidden;';
+
+    }
+}); */
+
